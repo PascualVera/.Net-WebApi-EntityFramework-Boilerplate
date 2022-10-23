b73b613a1e3bdda807e2b7d9f638975198266e12
PascualVera <pascualvera.994@gmail.com>
2022-10-23 23:00:13 +0200

    initial boilerplate created

---
+++ Program.cs
@@ -0,0 +1,37 @@
+global using webapi.Models;
+using webapi.Data;
+
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<DataContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
+//builder.Services.AddScoped<ICharacterService, CharacterService>(); => Example of how to add services to the program
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+  app.UseSwagger();
+  app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
